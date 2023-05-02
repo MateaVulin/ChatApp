694f9a8e02acd9c4d879b451def2e39ed7024913
Matea Vulin <matea.karadza1@gmail.com>
2023-05-02 17:23:54 +0200

    imput komponenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,226 @@
 import { Component } from "react";
 import Messages from "./components/Messages";
+import Input from "./components/Input";
 
 import "./App.css";
 
-function App() {
-  return;
+function randomName() {
+  const adjectives = [
+    "autumn",
+    "hidden",
+    "bitter",
+    "misty",
+    "silent",
+    "empty",
+    "dry",
+    "dark",
+    "summer",
+    "icy",
+    "delicate",
+    "quiet",
+    "white",
+    "cool",
+    "spring",
+    "winter",
+    "patient",
+    "twilight",
+    "dawn",
+    "crimson",
+    "wispy",
+    "weathered",
+    "blue",
+    "billowing",
+    "broken",
+    "cold",
+    "damp",
+    "falling",
+    "frosty",
+    "green",
+    "long",
+    "late",
+    "lingering",
+    "bold",
+    "little",
+    "morning",
+    "muddy",
+    "old",
+    "red",
+    "rough",
+    "still",
+    "small",
+    "sparkling",
+    "throbbing",
+    "shy",
+    "wandering",
+    "withered",
+    "wild",
+    "black",
+    "young",
+    "holy",
+    "solitary",
+    "fragrant",
+    "aged",
+    "snowy",
+    "proud",
+    "floral",
+    "restless",
+    "divine",
+    "polished",
+    "ancient",
+    "purple",
+    "lively",
+    "nameless",
+  ];
+  const nouns = [
+    "waterfall",
+    "river",
+    "breeze",
+    "moon",
+    "rain",
+    "wind",
+    "sea",
+    "morning",
+    "snow",
+    "lake",
+    "sunset",
+    "pine",
+    "shadow",
+    "leaf",
+    "dawn",
+    "glitter",
+    "forest",
+    "hill",
+    "cloud",
+    "meadow",
+    "sun",
+    "glade",
+    "bird",
+    "brook",
+    "butterfly",
+    "bush",
+    "dew",
+    "dust",
+    "field",
+    "fire",
+    "flower",
+    "firefly",
+    "feather",
+    "grass",
+    "haze",
+    "mountain",
+    "night",
+    "pond",
+    "darkness",
+    "snowflake",
+    "silence",
+    "sound",
+    "sky",
+    "shape",
+    "surf",
+    "thunder",
+    "violet",
+    "water",
+    "wildflower",
+    "wave",
+    "water",
+    "resonance",
+    "sun",
+    "wood",
+    "dream",
+    "cherry",
+    "tree",
+    "fog",
+    "frost",
+    "voice",
+    "paper",
+    "frog",
+    "smoke",
+    "star",
+  ];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    // this.drone = new window.Scaledrone("YOUR-CHANNEL-ID", {
+    //   data: this.state.member
+    // });
+    this.drone = new window.Scaledrone("qvMvGLfWONMhcKeX", {
+      data: this.state.member,
+    });
+    this.drone.on("open", (error) => {
+      if (error) {
+        return console.error(error);
+      }
+      const member = { ...this.state.member };
+      member.id = this.drone.clientId;
+      this.setState({ member });
+    });
+    const room = this.drone.subscribe("observable-room");
+    room.on("data", (data, member) => {
+      const messages = this.state.messages;
+      messages.push({ member, text: data });
+      this.setState({ messages });
+    });
+  }
+
+  // state = {
+  //   messages: [
+  //     {
+  //       text: "This is a test message!",
+  //       member: {
+  //         color: "blue",
+  //         username: "bluemoon"
+  //       }
+  //     }
+  //   ],
+  //   member: {
+  //     username: randomName(),
+  //     color: randomColor()
+  //   }
+  // }
+
+  state = {
+    messages: [],
+    member: {
+      username: randomName(),
+      color: randomColor(),
+    },
+  };
+
+  // onSendMessage = (message) => {
+  //   const messages = this.state.messages
+  //   messages.push({
+  //     text: message,
+  //     member: this.state.member
+  //   })
+  //   this.setState({messages: messages})
+  // }
+
+  onSendMessage = (message) => {
+    this.drone.publish({
+      room: "observable-room",
+      message,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
