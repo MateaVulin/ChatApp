f921e46a8f9bcf815be2e30eab771943ec960de6
Matea Vulin <matea.karadza1@gmail.com>
2023-05-15 21:55:06 +0200

    dodana nova komponenta

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Navbar() {
+  const [theme, setTheme] = useState('light');
+
+  const handleThemeChange = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    if (newTheme === 'dark') {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  };
+
+  return (
+    <nav className={`navbar ${theme === 'dark' ? 'dark-mode' : ''}`}>
+      <ul className="navbar-title"><h1><i>My first chat app </i> </h1>
+        <div className="navbar-item navbar-toggle">
+          <button id="dark-mode-toggle" className="dark-mode-button" onClick={handleThemeChange}>
+            Promijeni temu
+          </button>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
